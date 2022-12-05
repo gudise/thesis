f55c92a815338be887cbb52560c49572eda88f35
gds <gds@unizar.es>
2022-12-05 11:25:30 +0100

    He eliminado funciones que no se uilizan en este proyecto

--- c/include/main.h
+++ c/include/main.h
@@ -1,35 +1,10 @@
-/*
- * NOTAS:   . Las funciones MPFR_free() no funcionan muy bien
- * */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
 #include <string.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <omp.h>
-#include <gmp.h>
-#include <mpfr.h>
-#include "alias/mpfr.h" //redefinicion de las funciones mpfr
-#include "alias/gsl.h"
  
-
-#define IA 16807
-#define IM 2147483647
-#define AM (1.0/IM)
-#define IQ 127773
-#define IR 2836
-#define NTAB 32
-#define NDIV (1+(IM-1)/NTAB)
 #define EPS 1.2e-7
-#define RNMX (1.0-EPS)
-
-#define M_TAU 2*M_PI //TAU = 2*PI
-
 #define MTZ_INDEX(m, i, j)  i*m->N_columnas+j   
 #define MTZ_FIL_INDEX(m, n) n/m->N_columnas
 #define MTZ_COL_INDEX(m, n) n%m->N_columnas
@@ -37,34 +12,9 @@
 #define fseekset(x, y)  fseek(x, y, SEEK_SET)
 
 
-
-gsl_rng* idum;
 int percent;
-int indexec; //esta variable contiene el indice de ejecucion del programa (i.e. el indice de la iteracion, en caso de que se este llamando iterativamente). Para que un programa deje traza de su indice y por tanto pueda ser recogida por una iteracion posterior se utiliza el flag especial '!'
-
-void beforeMain(void) __attribute__((constructor));
-void beforeMain(void)
-{
-    FILE* punte;
-    int aux=0;
-    
-    punte=fopen(".indexec", "r");
-    if(punte)
-    {
-        fscanf(punte, "%d", &aux);
-        fclose(punte);
-    }
-    
-    indexec=aux+1;
-    
-    idum = gsl_rng_alloc(gsl_rng_taus);
-    gsl_rng_set(idum, (unsigned long) time(NULL)+indexec); //precision de SEGUNDOS!
-        
-}//en esta funcion se ejecutan las inicializaciones globales previas a 'main'. OJO! Esto SOLO FUNCIONA CON EL COMPILADOR gcc!
-
 
 /*ESTRUCTURAS*/
-
 typedef struct MATRIZ_STRUCT
 {
     double** elemento;
@@ -72,16 +22,6 @@ typedef struct MATRIZ_STRUCT
     unsigned long N_columnas;
 
 } MATRIZ;
-
-typedef gsl_matrix GSL_MATRIZ;
-
-typedef struct MPFR_MATRIZ_STRUCT
-{
-	mpfr_t** elemento;
-	unsigned long N_filas;
-	unsigned long N_columnas;
-
-} MPFR_MATRIZ;
 
 typedef struct TABLA_STRUCT
 {
@@ -101,14 +41,6 @@ typedef struct DISTRIBUCION_STRUCT
 
 } DISTRI;
 
-typedef struct MPFR_DISTRIBUCION_STRUCT
-{
-    unsigned long N;
-    mpfr_t* ejex;
-    mpfr_t* ejey;
-
-} MPFR_DISTRI;
-
 typedef struct SDISTRIBUCION_STRUCT
 {
     unsigned long N, M;
@@ -121,31 +53,22 @@ typedef struct SDISTRIBUCION_STRUCT
 } SDISTRI;
 
 
-
 /*ENCABEZADOS*/
 
 //gestion de memoria
 MATRIZ*         mallocMatriz(unsigned long N_filas, unsigned long N_columnas);
 
-MPFR_MATRIZ*    MPFR_mallocMatriz(unsigned long N_filas, unsigned long N_columnas);
-
 TABLA*          mallocTabla(unsigned long N_filas, unsigned long N_columnas);
 
 DISTRI*         mallocDistribucion(unsigned long N);
-
-MPFR_DISTRI*    MPFR_mallocDistribucion(unsigned long N);
 
 SDISTRI*        mallocSDistribucion(unsigned long N, unsigned long M);
 
 void            freeMatriz(MATRIZ* A);
 
-void 	        MPFR_freeMatriz(MPFR_MATRIZ* A);
-
 void            freeTabla(TABLA* A);
 
 void            freeDistribucion(DISTRI* result);
-
-void	        MPFR_freeDistribucion(MPFR_DISTRI* A);
 
 void            freeSDistribucion(SDISTRI* result);
 
@@ -185,27 +108,7 @@ void    quickSort_distribucion(DISTRI* data, int low, int high);
 
 void    ordenarData(MATRIZ* data); //ordena el vector columna 'data' de menor a mayor.
 
-void    GSL_ordenarData(MATRIZ* data); //ordena el vector columna 'data' de menor a mayor.
-
 void    ordenarData_distribucion(DISTRI* data);
-
-void    GSL_ordenarData_distribucion(DISTRI* data);
-
-int     MPFR_quickSort_particion(MPFR_MATRIZ* data, int low, int high);
-
-void    MPFR_quickSort(MPFR_MATRIZ* data, int low, int high);
-
-void    MPFR_ordenarData(MPFR_MATRIZ* data);//ordena el vector 'data' de menor a mayor usando el algoritmo quickSort
-
-void    MPFR_GSL_ordenarData(MPFR_MATRIZ* data); 
-
-int     MPFR_quickSort_particion_distribucion(MPFR_DISTRI* data, int low, int high);
-
-void    MPFR_quickSort_distribucion(MPFR_DISTRI* data, int low, int high);//esta funcion ordena la distribucion 'data' dirigida por la columna 'ejey'
-
-void    MPFR_ordenarData_distribucion(MPFR_DISTRI* data);
-
-void    MPFR_GSL_ordenarData_distribucion(MPFR_DISTRI* data);
 
 void    swap(double *x, double *y);
 
@@ -217,13 +120,7 @@ char    wordIsInSet(char* word, char* diccionario);
 //lectura
 MATRIZ*         leerMatriz(char* file_name);
 
-MPFR_MATRIZ*    MPFR_leerMatriz(char* file_name);
-
 TABLA*          leerTabla(char* file_name);
-
-gsl_matrix*     GSL_leerMatriz(FILE* punte);
-
-MPFR_DISTRI*    MPFR_leerDistribucion(char* file_name, char encabezado);
 
 void            leerParametrosTabla(long* N_filas_out, long* N_columnas_out, char* input, char encabezado);
 
@@ -231,13 +128,9 @@ void            leerParametrosTabla(long* N_filas_out, long* N_columnas_out, cha
 //escritura
 void    pintarMatriz(MATRIZ *A, FILE* punte);
 
-void    GSL_pintarMatriz(gsl_matrix *A, FILE* punte);
-
 void    pintarTabla(TABLA* tabla, char* fname);
 
 void    pintarDistribucion(DISTRI *blanco, char* output);
-
-void    MPFR_pintarDistribucion(MPFR_DISTRI* blanco, char* output);//pinta la distribucion. Si 'invertir' pinta la imagen especular de la distribucion
 
 void    animacionGnuplot(char* input, int N_frames, int N_dim, int N_repeticiones);
 
@@ -260,26 +153,6 @@ MATRIZ* mallocMatriz(unsigned long N_filas, unsigned long N_columnas)
         result->elemento[i] = calloc(N_columnas, sizeof(double));
 
     return result;
-}
-
-MPFR_MATRIZ* MPFR_mallocMatriz(unsigned long N_filas, unsigned long N_columnas)
-{
-	unsigned long i, j;
-	MPFR_MATRIZ* result;
-
-	result = (MPFR_MATRIZ*)malloc(sizeof(MPFR_MATRIZ));
-
-	result->N_filas = N_filas;
-	result->N_columnas = N_columnas;
-	result->elemento = (mpfr_t**)malloc(sizeof(mpfr_t*)*N_filas);
-	for(i=0; i<N_filas; i++)
-	{
-		result->elemento[i] = (mpfr_t*)malloc(sizeof(mpfr_t)*N_columnas);
-		for(j=0; j<N_columnas; j++)
-			mpfra_init_set_ui(result->elemento[i][j], 0);
-	}	
-	
-	return result;
 }
 
 TABLA* mallocTabla(unsigned long N_filas, unsigned long N_columnas)
@@ -320,27 +193,6 @@ DISTRI* mallocDistribucion(unsigned long N)
     return result;
 }
 
-MPFR_DISTRI* MPFR_mallocDistribucion(unsigned long N)
-{
-    unsigned long i;
-    MPFR_DISTRI* result;
-
-    result = (MPFR_DISTRI*)malloc(sizeof(MPFR_DISTRI));
-
-    result ->N = N;
-    result->ejex = (mpfr_t*)malloc(sizeof(mpfr_t)*N);
-    result->ejey = (mpfr_t*)malloc(sizeof(mpfr_t)*N);
-
-    for(i=0;i<N;i++)
-	{
-		mpfra_init_set_ui(result->ejex[i], 0);
-        
-		mpfra_init_set_ui(result->ejey[i], 0); 
-	}
-	
-    return result;
-}
-
 SDISTRI* mallocSDistribucion(unsigned long N, unsigned long M)
 {
     unsigned long i, j;
@@ -374,20 +226,6 @@ void freeMatriz(MATRIZ* A)
     free(A);
 }
 
-void MPFR_freeMatriz(MPFR_MATRIZ* A)
-{
-	int i, j;
-	for(i=0; i<A->N_filas; i++)
-	{
-		for(j=0; j<A->N_columnas; j++)
-			mpfr_clear(A->elemento[i][j]);
-	}
-	for(i=0; i<A->N_filas; i++)
-		free(A->elemento[i]);
-	free(A->elemento);
-	free(A);
-}
-
 void freeTabla(TABLA* A)
 {
     int i;
@@ -408,20 +246,6 @@ void freeDistribucion(DISTRI* result)
     free(result->ejex);
     free(result->ejey);
     free(result);
-}
-
-void MPFR_freeDistribucion(MPFR_DISTRI* A)
-{
-	int i;
-	for(i=0; i<A->N; i++)
-	{
-		mpfr_clear(A->ejex[i]);
-		mpfr_clear(A->ejey[i]);
-	}
-	free(A->ejex);
-	free(A->ejey);
-	free(A);
-
 }
 
 void freeSDistribucion(SDISTRI* result)
@@ -592,39 +416,6 @@ void percentage(int index, int N_total, char frase[256])
     }
 }
 
-double ran1()
-{
-    return gsl_rng_uniform(idum);
-}
-
-
-double ran1_gauss(double mu, double sigma)
-{	
-	return gsl_ran_gaussian(idum, sigma)+mu;
-}
-
-
-double   ran1_lineal(double parametro)
-{
-    double result;
-    double x;
-    
-    x = ran1();
-    
-    if(parametro<EPS) result = x;
-    
-    else result = (parametro-1)/(2*parametro) + sqrt(((1-parametro)*(1-parametro))/(4*parametro*parametro) + x/parametro);
-    
-    return result;
-}
-
-
-unsigned long dado(unsigned long N_caras)
-{
-    return gsl_rng_uniform_int(idum, N_caras);
-}
-
-
 void swap(double *x, double *y)
 {
     double temp;
@@ -672,25 +463,6 @@ void ordenarData(MATRIZ* data)
     quickSort(data, 0, data->N_filas-1);
 }
 
-
-void GSL_ordenarData(MATRIZ* data)
-{
-    long i;
-    double* p;
-    
-    p = malloc(sizeof(double)*data->N_filas);
-    for(i=0; i<data->N_filas; i++)
-        p[i]=data->elemento[i][0];
-    
-    gsl_heapsort(p, data->N_filas, sizeof(p[0]), (gsl_comparison_fn_t)compararDouble);
-    
-    for(i=0; i<data->N_filas; i++)
-        data->elemento[i][0] = p[i];
-    
-    free(p);
-}
-
-
 int quickSort_particion_distribucion(DISTRI* data, int low, int high)
 {
     int i, j;
@@ -729,162 +501,6 @@ void ordenarData_distribucion(DISTRI* data)
 {
     quickSort_distribucion(data, 0, data->N-1);
 }
-
-
-void GSL_ordenarData_distribucion(DISTRI* data)
-{
-    long i;
-    DISTRI* copia;
-    size_t* p;
-    
-    copia = mallocDistribucion(data->N);
-    for(i=0; i<data->N; i++)
-    {
-        copia->ejex[i] = data->ejex[i];
-        copia->ejey[i] = data->ejey[i];
-    }
-    
-    p = malloc(sizeof(size_t)*data->N);
-    
-    gsl_heapsort_index(p, copia->ejey, data->N, sizeof(data->ejey[0]), (gsl_comparison_fn_t)compararDouble);
-    
-    for(i=0; i<data->N; i++)
-    {
-        data->ejex[i]=copia->ejex[p[i]];
-        data->ejey[i]=copia->ejey[p[i]];
-    }
-    
-    freeDistribucion(copia);
-    free(p);
-}
-
-
-int MPFR_quickSort_particion(MPFR_MATRIZ* data, int low, int high)
-{
-    int i, j;
-    
-    i=low-1;
-    for(j=low; j<high; j++)
-    {
-        if(mpfr_less_p(data->elemento[j][0], data->elemento[high][0]))
-        {
-            i++;
-            mpfr_swap(data->elemento[i][0], data->elemento[j][0]);
-        }
-    }
-    mpfr_swap(data->elemento[i+1][0], data->elemento[high][0]);
-    
-    return (i+1);
-}
-
-void MPFR_quickSort(MPFR_MATRIZ* data, int low, int high)
-{
-    int p;
-    
-    if(low<high)
-    {
-        p = MPFR_quickSort_particion(data, low, high);
-        
-        MPFR_quickSort(data, low, p-1);
-        MPFR_quickSort(data, p+1, high);
-    }
-}
-
-
-void MPFR_ordenarData(MPFR_MATRIZ* data)
-{
-    MPFR_quickSort(data, 0, data->N_filas-1);
-}
-
-
-void MPFR_GSL_ordenarData(MPFR_MATRIZ* data)
-{
-    long i;
-    mpfr_t* p;
-    
-    p = malloc(sizeof(mpfr_t)*data->N_filas);
-    for(i=0; i<data->N_filas; i++)
-        mpfra_init_set(p[i], data->elemento[i][0]);
-    
-    gsl_heapsort(p, data->N_filas, sizeof(p[0]), (gsl_comparison_fn_t)mpfr_cmp);
-    
-    for(i=0; i<data->N_filas; i++)
-        mpfra_set(data->elemento[i][0], p[i]);
-    
-    for(i=0; i<data->N_filas; i++)
-        mpfr_clear(p[i]);
-    free(p);
-}
-
-
-int MPFR_quickSort_particion_distribucion(MPFR_DISTRI* data, int low, int high)
-{
-    int i, j;
-    
-    i=low-1;
-    for(j=low; j<high; j++)
-    {
-        if(mpfr_less_p(data->ejey[j], data->ejey[high]))
-        {
-            i++;
-            mpfr_swap(data->ejey[i], data->ejey[j]);
-            mpfr_swap(data->ejex[i], data->ejex[j]);
-        }
-    }
-    mpfr_swap(data->ejey[i+1], data->ejey[high]);
-    mpfr_swap(data->ejex[i+1], data->ejex[high]);
-    
-    return (i+1);
-}
-
-
-void MPFR_quickSort_distribucion(MPFR_DISTRI* data, int low, int high)
-{
-    int p;
-    
-    if(low<high)
-    {
-        p = MPFR_quickSort_particion_distribucion(data, low, high);
-        
-        MPFR_quickSort_distribucion(data, low, p-1);
-        MPFR_quickSort_distribucion(data, p+1, high);
-    }
-}
-
-
-void MPFR_ordenarData_distribucion(MPFR_DISTRI* data)
-{
-    MPFR_quickSort_distribucion(data, 0, data->N-1);
-}
-
-
-void MPFR_GSL_ordenarData_distribucion(MPFR_DISTRI* data)
-{
-    long i;
-    MPFR_DISTRI* copia;
-    size_t* p;
-    
-    copia = MPFR_mallocDistribucion(data->N);
-    for(i=0; i<data->N; i++)
-    {
-        mpfra_set(copia->ejex[i], data->ejex[i]);
-        mpfra_set(copia->ejey[i], data->ejey[i]);
-    }
-    
-    p = malloc(sizeof(size_t)*data->N);
-    
-    gsl_heapsort_index(p, copia->ejey, data->N, sizeof(data->ejey[0]), (gsl_comparison_fn_t)mpfr_cmp);
-    
-    for(i=0; i<data->N; i++)
-    {
-        mpfra_set(data->ejex[i], copia->ejex[p[i]]);
-        mpfra_set(data->ejey[i], copia->ejey[p[i]]);
-    }
-    
-    MPFR_freeDistribucion(copia);
-    free(p);
-}
-
 
 void delay(double seconds)
 {
@@ -946,38 +562,6 @@ MATRIZ* leerMatriz(char* file_name)
     return result;
 }
 
-MPFR_MATRIZ* MPFR_leerMatriz(char* file_name)
-{
-    int i, j, n, m;
-    MPFR_MATRIZ* result;
-    char basura[256];
-    FILE* punte;
-    
-    punte = fopen(file_name, "r");
-    buscarPalabra(punte, "#[N_filas]");
-    fscanf(punte, "%d", &n);
-
-    buscarPalabra(punte, "#[N_columnas]");
-    fscanf(punte, "%d", &m);
-
-    result = MPFR_mallocMatriz(n, m);
-
-    buscarPalabra(punte, "#[matriz]");
-    
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<m; j++)
-        {
-            fscanf(punte, "%s", basura);
-            mpfra_set_str(result->elemento[i][j], basura, 10);
-        }
-    }
-    fclose(punte);
-    
-    return result;
-}
-
-
 TABLA* leerTabla(char* file_name)
 {
     int i, j, N_filas=0, N_columnas=1;
@@ -1021,112 +605,6 @@ TABLA* leerTabla(char* file_name)
 
     return result;
 }
-
-
-gsl_matrix* GSL_leerMatriz(FILE* punte)
-{
-    ///Esta funcion lee un archivo .mtz; aunque el formato '.mtz' especifica el nombre de los parametros [N_filas] y [N_columnas], la funcion es flexible y permite leer una variedad de nombres similares (ver abajo)
-    
-    int n, m, i, j;
-    gsl_matrix* result;
-    double aux;
-        
-    if(buscarPalabra(punte, "#[N_filas]"))
-        fscanf(punte, "%d", &n);
-    else if(buscarPalabra(punte, "#[Nfilas]"))
-        fscanf(punte, "%d", &n);
-    else if(buscarPalabra(punte, "#[n_filas]"))
-        fscanf(punte, "%d", &n);
-    else if(buscarPalabra(punte, "#[nfilas]"))
-        fscanf(punte, "%d", &n);
-    else if(buscarPalabra(punte, "#[filas]"))
-        fscanf(punte, "%d", &n);
-
-    if(buscarPalabra(punte, "#[N_columnas]"))
-        fscanf(punte, "%d", &m);
-    else if(buscarPalabra(punte, "#[Ncolumnas]"))
-        fscanf(punte, "%d", &m);
-    else if(buscarPalabra(punte, "#[n_columnas]"))
-        fscanf(punte, "%d", &m);
-    else if(buscarPalabra(punte, "#[ncolumnas]"))
-        fscanf(punte, "%d", &m);
-    else if(buscarPalabra(punte, "#[columnas]"))
-        fscanf(punte, "%d", &m);
-
-    result = gsl_matrix_calloc(n, m);
-
-    buscarPalabra(punte, "#[matriz]");
-    
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<m; j++)
-        {
-            fscanf(punte, "%lg", &aux);
-            gsl_matrix_set(result, i, j, aux);
-        }
-    }
-    
-    return result;
-}
-
-
-MPFR_DISTRI* MPFR_leerDistribucion(char* file_name, char encabezado)
-{
-    long N=-1, i;
-    char basura[256];
-    MPFR_DISTRI* result;
-    FILE* punte;
-    
-    punte = fopen(file_name, "r");
-    
-    while(!feof(punte))
-    {
-        fscanf(punte, "%s", basura);
-        fscanf(punte, "%s", basura);
-        N++;
-    }
-    fclose(punte);
-    
-    punte = fopen(file_name, "r");
-    
-    if(encabezado)
-    {
-        N--;
-        
-        result = MPFR_mallocDistribucion(N);
-        
-        fscanf(punte, "%s", basura);
-        fscanf(punte, "%s", basura);
-        
-        for(i=0; i<N; i++)
-        {
-            fscanf(punte, "%s", basura);
-            mpfra_set_str(result->ejex[i], basura, 10);
-            
-            fscanf(punte, "%s", basura);
-            mpfra_set_str(result->ejey[i], basura, 10);
-        }
-    }
-    
-    else
-    {
-        result = MPFR_mallocDistribucion(N);
-        
-        for(i=0; i<N; i++)
-        {
-            fscanf(punte, "%s", basura);
-            mpfra_set_str(result->ejex[i], basura, 10);
-            
-            fscanf(punte, "%s", basura);
-            mpfra_set_str(result->ejey[i], basura, 10);
-        }
-    }
-    
-    fclose(punte);
-    
-    return result;
-}
-
 
 void leerParametrosTabla(long* N_filas_out, long* N_columnas_out, char* input, char encabezado)
 {
@@ -1177,31 +655,6 @@ void pintarMatriz(MATRIZ *A, FILE* punte)
     fprintf(punte, "#[fin]\n");
 }
 
-void GSL_pintarMatriz(gsl_matrix *A, FILE* punte)
-{
-    ///Esta funcion escribe una matriz en formato .mtz
-    
-    int n, m, i, j;
-    double aux;
-    
-    n=(int)(A->size1);
-    m=(int)(A->size2);
-    
-    fprintf(punte, "#[N_filas]\t%d\n#[N_columnas]\t%d\n\n#[matriz]\n", n, m);
-    
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<m; j++)
-        {
-            aux = gsl_matrix_get(A, i, j);
-            fprintf(punte, "%.17lg\t", aux);
-        }
-        fprintf(punte, "\n");
-    }
-    fprintf(punte, "#[fin]\n");
-}
-
-
 void pintarTabla(TABLA* tabla, char* file_name)
 {
     int i, j;
@@ -1238,25 +691,6 @@ void pintarDistribucion(DISTRI *blanco, char* output)
 
     fclose(punte);
 }
-
-
-void MPFR_pintarDistribucion(MPFR_DISTRI* blanco, char* output)
-{
-    int i;
-    FILE* punte;
-    
-    punte = fopen(output, "w");
-    fprintf(punte, "x\ty\n");
-    for(i=0; i<blanco->N; i++)
-    {
-        mpfra_out_str(punte, 10, 6, blanco->ejex[i]);
-        fprintf(punte, "\t");
-
-        mpfra_out_str(punte, 10, 6, blanco->ejey[i]);
-        fprintf(punte, "\n");
-    }
-}
-
 
 void animacionGnuplot(char* input, int N_frames, int N_dim, int N_repeticiones)
 {
