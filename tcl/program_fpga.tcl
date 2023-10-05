a1bdab2ad83c2671012aabce2d9e48161b6ca4ed
gds <gds@unizar.es>
2023-10-05 13:46:58 +0200

    Salvo el 'fpga_program_all', lo demás parece funcionar.

--- tcl/program_fpga.tcl
+++ tcl/program_fpga.tcl
@@ -1,9 +1,8 @@
 ## Este scrip programa un SoC Zynq-7000 con un bitstream, y programa el micro con una aplicación software. Está diseñado para ser utilizado junto con la función 'design' del módulo 'ring_osc'. Requiere un directorio 'sdk' tal y como es creado por Vivado, con un proyecto hardware y una aplicación software. Se dan por hecho que los nombres de los proyectos y ficheros son los dados por defecto.
 
-## Deben pasarse OBLIGATORIAMENTE dos parámetros a este script: el primero ha de ser el directorio del proyecto SDK; el segundo, el nombre de la aplicación.
+## Debe pasarse OBLIGATORIAMENTE un parámetro a este script: el directorio del proyecto SDK.
 
 set sdkdir [lindex $argv 0]
-
 set appname [lindex $argv 1]
 
 
@@ -15,11 +14,12 @@ rst
 
 fpga ${sdkdir}/design_1_wrapper_hw_platform_0/design_1_wrapper.bit
 
-loadhw ${sdkdir}/design_1_wrapper_hw_platform_0/system.hdf
+#loadhw ${sdkdir}/design_1_wrapper_hw_platform_0/system.hdf
 
 source ${sdkdir}/design_1_wrapper_hw_platform_0/ps7_init.tcl
 
 ps7_init
+
 ps7_post_config
 
 dow ${sdkdir}/${appname}/Debug/${appname}.elf
