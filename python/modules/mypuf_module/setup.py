8c54905531881ca0c1e98dbcdee1ed2ce0bc83c1
gds <gds@unizar.es>
2024-01-05 17:26:29 +0100

    Migración del repo 'puf' a 'thesis/puf'.

--- python/modules/mypuf_module/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='mypuf',
-      version='2.2',
-      description='Módulo de Python para el proyecto puf.',
-      author='gds',
-      packages=['mypuf'],
-      zip_safe=False)
